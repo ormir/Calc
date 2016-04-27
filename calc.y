6e01850b591eb9d918e1cbfe14367fbca1fd1a17
NitikaK <dollysharma@dollys-MacBook-Air.local>
2016-04-27 17:51:16 +0200

    #coolstuff

---
+++ calc.y
@@ -0,0 +1,71 @@
+%{
+#include <stdio.h>
+int sym[26];
+int yyerror(char *s); 
+int yylex();
+int min (int a,int b);
+int max (int a, int b);
+%}
+
+%token VARIABLE ASSIGN INTEGER NEWLINE LBRACE RBRACE SMALLER SMALLEREQ EQUAL DIFFERENT GREATER GREATEREQ COMMA MIN MAX
+%left PLUS
+%left TIMES
+%left MINUS
+%left DIVIDE
+%left MODULO
+
+
+%%
+
+program: program statement
+       | 
+       ;
+
+statement: expr NEWLINE 
+	     { printf("%d\n", $1); }
+         | VARIABLE ASSIGN expr NEWLINE
+             { sym[$1] = $3; }
+         ;
+
+expr: INTEGER            { $$ = $1; }
+      | VARIABLE         { $$ = sym[$1]; }
+      | expr PLUS expr   { $$ = $1 + $3; }
+      | expr TIMES expr  { $$ = $1 * $3; }
+      | expr MINUS expr  { $$ = $1 - $3; }
+      | expr DIVIDE expr  { $$ = $1 / $3; }
+      | expr MODULO expr  { $$ = $1 % $3; }
+      | LBRACE expr RBRACE { $$ = $2 ; }
+      | MINUS expr { $$ = - $2 ; }
+      | PLUS expr { $$ = $2; }
+      | expr SMALLER expr { $$ = $1 < $3; }
+      | expr SMALLEREQ expr { $$ = $1 <= $3; }
+      | expr EQUAL expr { $$ = $1 == $3; }
+      | expr DIFFERENT expr { $$ = $1 != $3; }
+      | expr GREATER expr { $$ = $1 > $3; }
+      | expr GREATEREQ expr { $$ = $1 >= $3; }
+      | MIN LBRACE expr COMMA expr RBRACE { $$ = min ( $3, $5); }
+      | MAX LBRACE expr COMMA expr RBRACE { $$ = max ($3,$5); }
+
+
+      ;
+
+%%
+
+int yyerror(char *s) {
+  fprintf(stderr, "%s\n", s);
+  return 0;
+}
+
+int min (int a,int b){
+  return (a < b) ? a : b;
+}
+
+int max (int a, int b){
+  return (a > b) ? a : b;
+}
+
+
+int main() {
+  yyparse();
+  return 0;
+}
---
+++ y.tab.h
@@ -0,0 +1,97 @@
+/* A Bison parser, made by GNU Bison 2.3.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     VARIABLE = 258,
+     ASSIGN = 259,
+     INTEGER = 260,
+     NEWLINE = 261,
+     LBRACE = 262,
+     RBRACE = 263,
+     SMALLER = 264,
+     SMALLEREQ = 265,
+     EQUAL = 266,
+     DIFFERENT = 267,
+     GREATER = 268,
+     GREATEREQ = 269,
+     COMMA = 270,
+     MIN = 271,
+     MAX = 272,
+     PLUS = 273,
+     TIMES = 274,
+     MINUS = 275,
+     DIVIDE = 276,
+     MODULO = 277
+   };
+#endif
+/* Tokens.  */
+#define VARIABLE 258
+#define ASSIGN 259
+#define INTEGER 260
+#define NEWLINE 261
+#define LBRACE 262
+#define RBRACE 263
+#define SMALLER 264
+#define SMALLEREQ 265
+#define EQUAL 266
+#define DIFFERENT 267
+#define GREATER 268
+#define GREATEREQ 269
+#define COMMA 270
+#define MIN 271
+#define MAX 272
+#define PLUS 273
+#define TIMES 274
+#define MINUS 275
+#define DIVIDE 276
+#define MODULO 277
+
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+
+extern YYSTYPE yylval;
+
