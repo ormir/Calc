9c2354224709e2c40a2b04c1cd173a7afc455c7b
Ormir G <ormirgj@gmail.com>
2016-04-27 12:54:57 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Calc
+//
+//  Created by Ormir G. on 27/04/16.
+//  Copyright © 2016 Ormir Gjurgjej, Nitika Kumar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
